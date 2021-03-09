ac7651e114a3ac33fb4c3ff722dee6ba3660370f
AVIN111 <avinash.patil18@st.niituniversity.in>
2021-03-09 19:28:17 +0530

    Intial Phase

---
+++ pharma/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Medicine:
+    id:int
+    name:str
+    img:str
+    price:int
---
+++ pharma/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from . models import Medicine
+# Create your views here.
+def index(request):
+    med1=Medicine()
+    med1.name='Crocin'
+    med1.price=100
+    return render(request,"index.html",{'med1':med1})
+
+def cart(request):
+    return render(request,"cart.html")
