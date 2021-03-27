650e2cf2b8769f059892933637b3ae26b4e184ab
sarthak248 <65854909+sarthak-banerjee@users.noreply.github.com>
2021-03-27 15:46:27 +0530

    models

--- pharma/models.py
+++ pharma/models.py
@@ -1,8 +1,23 @@
 from django.db import models
 
 # Create your models here.
+<<<<<<< Updated upstream
 class Medicine:
     id:int
     name:str
     img:str
     price:int
+=======
+class Medical_Products(models.Model):
+    med_name = models.CharField(max_length=100)
+    med_price = models.IntegerField()
+    med_img = models.ImageField(upload_to='pictures')
+    med_sale = models.BooleanField(default=False)
+    med_des = models.TextField()
+    med_category = models.CharField(max_length=25)
+    med_type = models.CharField(max_length=25) 
+
+class User_Cart(models.Model):
+    user_id = models.CharField(max_length=100)
+    med_id = models.CharField(max_length=100)
+>>>>>>> Stashed changes
