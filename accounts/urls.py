406764048421274e9fc0f96bf025950051760b8f
Avinash Yuvraj Patil <60743077+AVIN111@users.noreply.github.com>
2021-03-29 19:18:49 +0530

    updated path

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
+        path('accounts/', include('allauth.urls')),
         path("register", views.register, name="register"),
         path("login", views.login, name="login"),
         path("logout", views.logout, name='logout')
